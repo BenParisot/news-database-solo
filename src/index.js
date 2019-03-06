2bc66183db17a1b2442e1747fef88c465edbac06
Ben Parisot <me@benparisot.com>
2019-03-06 14:50:29 -0800

    implement API fetch and display results

--- src/index.js
+++ src/index.js
@@ -2,12 +2,20 @@ import { articles } from '../article-list.js';
 import loadArticles from './make-article-list.js';
 import updateSearchTerm from './search-component.js';
 import { queryToObject } from './hash-query.js';
+import { makeSearchArticlesURL } from './make-search-api-url.js';
 
-loadArticles(articles);
+// loadArticles(articles);
 updateSearchTerm();
 
 window.addEventListener('hashchange', () => {
     const query = window.location.hash.slice(1);
     const queryOptions = queryToObject(query);
     console.log(queryOptions);
+
+    const URL = makeSearchArticlesURL(queryOptions);
+    fetch(URL)
+        .then(response => response.json())
+        .then(response => {
+            loadArticles(response.articles);
+        });
 });
--- test/make-search-api-url.test.js
+++ test/make-search-api-url.test.js
@@ -1,16 +1,5 @@
+import { makeSearchArticlesURL } from '../src/make-search-api-url.js';
 const test = QUnit.test;
-
-const SEARCH_ARTICLE_URL = 'https://newsapi.org/v2/everything?';
-const API_KEY = '48464c7f112d4fc4a5047ae304c56c1d';
-
-function makeSearchArticlesURL(queryOptions) {
-    const url = new URL(SEARCH_ARTICLE_URL);
-    url.searchParams.set('q', queryOptions.q);
-    url.searchParams.set('apiKey', API_KEY);
-    url.searchParams.set('page', queryOptions.page);
-    url.searchParams.set('language', 'en');
-    return url.toString();
-}
 
 test('includes searchTerm and page in search movie url', assert => {
     //arange
