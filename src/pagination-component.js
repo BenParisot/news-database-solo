408f33456c928620faf3a725020938f64856c3fc
Ben Parisot <me@benparisot.com>
2019-03-06 16:56:13 -0800

    get pagination working with disabled previous/next buttons based on current/total pages

--- src/pagination-component.js
+++ src/pagination-component.js
@@ -5,9 +5,6 @@ const nextButtonNode = document.getElementById('next-button');
 const currentPageNode = document.getElementById('current-page');
 const totalPagesNode = document.getElementById('total-pages');
 
-let currentPage = 1;
-let totalPages = 6;
-
 //write function to update page numbers when hashchange event occurs
 
 //total pages needs to equal total results / 20
@@ -19,20 +16,33 @@ export function updatePaging(page, totalResults) {
     totalPagesNode.textContent = totalPages;
 }
 
+const existingQuery = window.location.hash.slice(1);
+const searchParams = new URLSearchParams(existingQuery);
+let currentPage = searchParams.get('page');
+previousButtonNode.disabled = currentPage <= 1;
+
 previousButtonNode.addEventListener('click', () => {
+    const totalPages = totalPagesNode.textContent;
     const existingQuery = window.location.hash.slice(1);
     const searchParams = new URLSearchParams(existingQuery);
-    const currentPage = searchParams.get('page');
-    console.log(existingQuery, currentPage);    
+    let currentPage = searchParams.get('page');
+    currentPage--;
+    previousButtonNode.disabled = currentPage <= 1;
+    nextButtonNode.disabled = currentPage >= totalPages;
+    const newURL = pageToURL(existingQuery, currentPage);
+    window.location.hash = newURL;   
 });
 
 nextButtonNode.addEventListener('click', () => {
+
+    const totalPages = totalPagesNode.textContent;
+
     const existingQuery = window.location.hash.slice(1);
     const searchParams = new URLSearchParams(existingQuery);
     let currentPage = searchParams.get('page');
     currentPage++;
+    previousButtonNode.disabled = currentPage <= 1;
+    nextButtonNode.disabled = currentPage >= totalPages;
     const newURL = pageToURL(existingQuery, currentPage);
-    window.location.hash = newURL;
-    console.log(existingQuery, currentPage);    
-
+    window.location.hash = newURL;   
 });
